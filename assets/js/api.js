6fc9c8f832a1de7bc92ae54ea826599c47724672
Kaidan <kaidangustave@yahoo.com>
2022-09-11 15:06:09 -0500

    finished up basic API script

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,32 +1,44 @@
 let apiKey = 'a0eace9ccff525be4601e59c9d29c184';
 let apiBase = 'http://api.openweathermap.org/';
+
 let directGeoRoute = 'geo/1.0/direct';
 let currentWeatherRoute = 'data/2.5/weather';
 
+let weatherTypes = {
+    clear: {
+        name: 'Clear Skies',
+        iconClass: 'fa-sun'
+    },
+    clouds: {
+        name: 'Clouds',
+        iconClass: 'fa-cloud'
+    },
+    rain: {
+        name: 'Rain',
+        iconClass: 'fa-cloud-rain'
+    },
+    snow: {
+        name: 'Snow',
+        iconClass: 'fa-snowflake'
+    },
+    fog: {
+        name: "Fog",
+        iconClass: 'fa-smog'
+    }
+};
+
+let convertKToF = (kelvin) => (1.8 * (kelvin - 273)) + 32;
 let formatURL = (route) => apiBase + route;
 let appendAppID = (requestUrl) => requestUrl + `&appid=${apiKey}`;
-let autoConvertPromiseToJSON = (response) => response.json();
+let convertPromiseToJSON = (response) => response.json();
 let verifyGood = (json) => {
+    // if the json is a good request or it's a JSON array
     if((json.cod >= 200 && json.cod < 400) || Array.isArray(json)) {
         return json;
     }
 
-    throw { reason: 'error with request', payload: json };
-}
-
-function __testDG() {
-    directGeocode({name: 'Buffalo Grove'})
-        .then((json) => {
-            if(json.length < 1) {
-                return;
-            }
-            let val = json[0];
-            currentWeather(val.lat, val.lon)
-                .then((json) => {
-                    console.log(json);
-                })
-        });
-}
+    throw {reason: 'error with request', payload: json};
+};
 
 function directGeocode(geoInfo) {
     let requestUrl = formatURL(directGeoRoute);
@@ -42,12 +54,37 @@ function directGeocode(geoInfo) {
     requestUrl += '&limit=1';
     requestUrl = appendAppID(requestUrl);
 
-    return fetch(requestUrl).then(autoConvertPromiseToJSON).then(verifyGood);
+    return fetch(requestUrl)
+        .then(convertPromiseToJSON)
+        .then(verifyGood)
+        .then((json) => {
+            if(json.length < 1) {
+                throw {reason: 'not found', payload: json};
+            }
+            return json;
+        });
 }
 
-function currentWeather(lat, lon) {
+function currentWeather(lat, lon, city) {
     let requestUrl = formatURL(currentWeatherRoute);
     requestUrl += `?lat=${lat}&lon=${lon}&units=standard`;
     requestUrl = appendAppID(requestUrl);
-    return fetch(requestUrl).then(autoConvertPromiseToJSON).then(verifyGood);
+    return fetch(requestUrl)
+        .then(convertPromiseToJSON)
+        .then(verifyGood)
+        .then((json) => {
+            let apiJson = {
+                loc: {lat: lat, lon: lon, city: city},
+                weather: json.weather,
+                temp: convertKToF(json.main.temp),
+                wind: json.wind.speed,
+                humidity: json.main.humidity
+            };
+            return apiJson; // rewrap this with all the info we need
+        });
+}
+
+function fetchWeather(city) {
+    return directGeocode({name: city})
+        .then((json) => currentWeather(json[0].lat, json[0].lon, json[0].name));
 }
